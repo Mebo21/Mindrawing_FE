23967d73f0ee82e2282aff98a2b01001e0741337
rktdnjs <rktdnjs@naver.com>
2024-08-26 01:27:36 +0900

    [feat] : 마이페이지 탭별 라우팅 추가

--- src/pages/myPage/UserMyPage.jsx
+++ src/pages/myPage/UserMyPage.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import { IoIosArrowForward } from 'react-icons/io';
+import { useNavigate } from 'react-router-dom';
+import routes from '../../constant/routes';
 
 const UserMyPage = () => {
+  const navigate = useNavigate();
   return (
     <MenuList>
-      <MenuItem>
+      <MenuItem onClick={() => navigate(routes.analysisHistory)}>
         그림 심리분석 내역 <IoIosArrowForward size={30} />
       </MenuItem>
-      <MenuItem>
+      <MenuItem onClick={() => navigate(routes.mypageUserInfoEdit)}>
         개인정보 수정 <IoIosArrowForward size={30} />
       </MenuItem>
-      <MenuItem>
+      <MenuItem onClick={() => navigate(routes.mypageChildInfoRegister)}>
         아이정보 등록 <IoIosArrowForward size={30} />
       </MenuItem>
-      <MenuItem>
+      <MenuItem onClick={() => navigate(routes.mypagePartnerCenterRegister)}>
         제휴센터 등록 <IoIosArrowForward size={30} />
       </MenuItem>
     </MenuList>
