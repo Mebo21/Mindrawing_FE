7c3c03793c522ecac783eb12f038a9271cf3788b
rktdnjs <rktdnjs@naver.com>
2024-08-25 23:58:03 +0900

    [chore] : Swal에서 사용할 메시지 추가

---
+++ src/utils/alert.js
@@ -0,0 +1,11 @@
+export const loginNeedMessage = {
+  title: '로그인이 필요한 서비스입니다!',
+  text: '로그인을 먼저 해주세요😊',
+  confirmButtonText: '확인',
+};
+export const authInvalidMessage = {
+  title: '해당 페이지의 접근 권한이 없습니다!',
+  text: '홈으로 이동합니다.',
+  icon: 'error',
+  confirmButtonText: '확인',
+};
