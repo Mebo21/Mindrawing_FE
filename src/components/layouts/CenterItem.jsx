d0722a5bbf91e8a0b8018461e2493826bae160c3
rktdnjs <rktdnjs@naver.com>
2024-08-27 01:18:25 +0900

    [style] : 센터 목록 센터별 레이아웃 스타일 조정

--- src/components/layouts/CenterItem.jsx
+++ src/components/layouts/CenterItem.jsx
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import sampleImage from '../../assets/images/Sample_image.png';
 import { useNavigate } from 'react-router-dom';
+import { FaRegHeart, FaHeart } from 'react-icons/fa'; // FaHeart 추가
 
 const CenterItem = () => {
   const navigate = useNavigate();
+  const [isFavorite, setIsFavorite] = useState(false); // 좋아요 상태 관리
+
+  const toggleFavorite = (e) => {
+    e.stopPropagation(); // 하트 클릭 시 센터 상세 페이지로 이동 방지
+    setIsFavorite(!isFavorite); // 상태 토글
+  };
 
   return (
     <CenterItemWrapper onClick={() => navigate('/centers/1')}>
       <ImageWrapper>
         <Image src={sampleImage} alt="센터 사진" />
       </ImageWrapper>
-      <CenterItemInfo>
+      <CenterLeftItemInfo>
         <Title>용봉 아동 심리센터</Title>
         <Address>광주광역시 북구 용주로 30번길 66</Address>
         <PhoneNumber>전화번호 : 010-0000-0000</PhoneNumber>
         <Info>영업시간 : 09:00 - 18:00</Info>
-      </CenterItemInfo>
+      </CenterLeftItemInfo>
+      <CenterRightItemInfo>
+        <HeartIcon onClick={toggleFavorite}>
+          {isFavorite ? <FaHeart color="red" size={30} /> : <FaRegHeart size={30} />}
+        </HeartIcon>
+        <OpenDiv>영업중</OpenDiv>
+      </CenterRightItemInfo>
     </CenterItemWrapper>
   );
 };
 
+// 스타일 정의
 const CenterItemWrapper = styled.div`
   display: flex;
+  justify-content: space-between;
   width: 100%;
   height: 150px;
   padding: 25px 0;
@@ -44,10 +59,18 @@ const Image = styled.img`
   border-radius: 10px;
 `;
 
-const CenterItemInfo = styled.div`
+const CenterLeftItemInfo = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+`;
+
+const CenterRightItemInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end; /* 오른쪽 정렬 */
+  padding: 10px 0;
 `;
 
 const Title = styled.h2`
@@ -68,6 +91,22 @@ const PhoneNumber = styled.p`
 
 const Info = styled.p`
   font-size: 10px;
+`;
+
+const OpenDiv = styled.div`
+  display: flex;
+  width: 50px;
+  height: 20px;
+  background-color: #8eff8b;
+  border-radius: 10px;
+  font-size: 10px;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+`;
+
+const HeartIcon = styled.div`
+  cursor: pointer;
 `;
 
 export default CenterItem;
