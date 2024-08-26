a5d43e810d8fc60698afe9b8916e016482c123ba
rktdnjs <rktdnjs@naver.com>
2024-08-26 17:22:27 +0900

    [feat] : 센터목록페이지 큰 레이아웃 잡기

--- src/pages/centerPage/CenterListPage.jsx
+++ src/pages/centerPage/CenterListPage.jsx
@@ -4,13 +4,35 @@ import PageTemplate from '../../components/templates/PageTemplate';
 import Loader from '../../components/layouts/Loader';
 import Nav from '../../components/layouts/Nav';
 import { useQuery } from '@tanstack/react-query';
+import CenterItem from './../../components/layouts/CenterItem';
 
 const CenterListPage = () => {
   return (
     <PageTemplate>
+      {/* width : 360px height : 100px 주변센터목록 상단 */}
+      {/* 카카오맵 API */}
+      {/* 구분선 */}
+      <CenterItemsContainer>
+        <CenterItem />
+        <CenterItem />
+        <CenterItem />
+        <CenterItem />
+        <CenterItem />
+      </CenterItemsContainer>
       <Nav currentPage="centerListPage" />
     </PageTemplate>
   );
 };
+
+const CenterItemsContainer = styled.div`
+  margin-top: 60px;
+  padding: 0 20px;
+  height: 300px;
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
 
 export default CenterListPage;
